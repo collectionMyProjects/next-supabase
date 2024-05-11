df009ab67d71a52fadd57623504c8d6a8b2d486a
lsj0202 <w1004123@gmail.com>
2024-05-11 14:15:53 +0900

    feat: add genTypes를 통해 table types 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { createClient } from '@supabase/supabase-js';
 
 const inter = Inter({ subsets: ['latin'] });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+import { createClient } from '@/utils/supabase/client';
 import Image from 'next/image';
 
+const supabase = createClient();
+
 export default function Home() {
+  supabase
+    .from('Test')
+    .select('*')
+    .then((res) => console.log(res.data));
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
