3f5c265fb79c953ccecee1f6925bf37926525f91
lsj0202 <w1004123@gmail.com>
2024-05-12 21:40:44 +0900

    feat: add sidebar component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,27 @@
+'use client';
+
+import { isSidebarOpen } from '@/atoms/sidebar';
+import { useAtom } from 'jotai';
 import Link from 'next/link';
-import { AiOutlineMenu } from 'react-icons/ai';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 import { BsRobot } from 'react-icons/bs';
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useAtom(isSidebarOpen);
+
   return (
     <header className="flex h-16 items-center justify-between border-b px-4 lg:px-10">
-      <button className="p-2">
-        <AiOutlineMenu className="size-5 lg:size-6" />
+      <button onClick={() => setIsOpen((tag) => !tag)} className="p-2">
+        {isOpen ? (
+          <AiOutlineClose className="size-5 lg:size-6" />
+        ) : (
+          <AiOutlineMenu className="size-5 lg:size-6" />
+        )}
       </button>
       <Link href="/">
         <h1 className="text-3xl font-medium text-slate-600">BLOG</h1>
       </Link>
-      <Link href="/posts">
+      <Link href="/search">
         <BsRobot className="size-5 lg:size-6" />
       </Link>
     </header>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,52 @@
+'use client';
+
 import { isSidebarOpen } from '@/atoms/sidebar';
 import { cn } from '@/utils/style';
 import { useAtom } from 'jotai';
+import Link from 'next/link';
+import { AiFillGithub, AiFillInstagram, AiOutlineClose } from 'react-icons/ai';
 
 const Sidebar = () => {
-  const [isOpen, setIsOepn] = useAtom(isSidebarOpen);
+  const [isOpen, setIsOpen] = useAtom(isSidebarOpen);
 
-  return <div className={cn('min-h-screen', isOpen ? 'flex' : 'hidden')}></div>;
+  return (
+    <div
+      className={cn(
+        'absolute z-10 min-h-screen flex-col gap-6 border-r bg-white p-10 text-base lg:relative',
+        isOpen ? 'flex' : 'hidden',
+      )}
+    >
+      <div className="flex justify-end lg:hidden">
+        <AiOutlineClose
+          className="size-6 cursor-pointer"
+          onClick={() => setIsOpen(false)}
+        />
+      </div>
+      <Link href="/" className="w-48 font-medium text-gray-600 hover:underline">
+        홈
+      </Link>
+      <Link
+        href="/tag"
+        className="w-48 font-medium text-gray-600 hover:underline"
+      >
+        태그
+      </Link>
+      <Link
+        href="/category/Web-Developmnet"
+        className="w-48 font-medium text-gray-600 hover:underline"
+      >
+        Web Development
+      </Link>
+      <div className="mt-10 flex items-center gap-4">
+        <Link href="https://www.instagram.com/lsj0202_" target="_blank">
+          <AiFillInstagram className="size-6" />
+        </Link>
+        <Link href="https://github.com/lsj0202" target="_blank">
+          <AiFillGithub className="size-6" />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
